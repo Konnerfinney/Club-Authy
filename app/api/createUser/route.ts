35bc8286ae14131a04b90d7d05ba0d44b794933d
konnerfinney <kfinney@trinitytg.com>
2023-11-14 16:20:28 -0800

    Added in new token verification function and duplicate checking and removal

--- app/api/createUser/route.ts
+++ app/api/createUser/route.ts
@@ -1,26 +1,14 @@
 // app/api/addUser/route.ts
 require('dotenv').config();
 const { randomBytes } = require('crypto');
-
 import clientPromise from '../../utils/mongodb';
-const nodemailer = require('nodemailer');
+import { generateVerificationToken } from '../../utils/authToken';
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    type: 'OAuth2',
-    user: 'konner.programming@gmail.com',
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-  },
-});
 export async function POST(request) {
 
   try {
     // Parse the incoming request body to get the user data
     const body = await request.json();
-    console.log(body);
     const { discordServerId, discordUserId, userName, userEmail, userComment } = body;
 
     
@@ -33,10 +21,11 @@ export async function POST(request) {
       });
     }
     const userAuthToken = generateVerificationToken();
+    console.log(userAuthToken);
     // Initialize the MongoDB client and connect to the database
     const client = await clientPromise;
     const db = client.db("Discord_Bot"); 
-
+    
     // Define the document to insert
     const doc = {
       discordServerId,
@@ -50,7 +39,15 @@ export async function POST(request) {
     //  
 
     // Insert the document into the collection
-    const collection = db.collection("Unauthenticated Users"); //
+    const collection = db.collection("users"); //
+    // Check if the user exists and then delete if it does to replace with new data
+    const userExists = await collection.findOne({ 
+      discordServerId, 
+      discordUserId 
+    });
+    if (!!userExists) {
+      await collection.deleteOne(userExists);
+    }
     const response = await collection.insertOne(doc);
 
     // Respond with success if the insertion was acknowledged
@@ -76,26 +73,4 @@ export async function POST(request) {
 }
 
 
-function generateVerificationToken() {
-  return randomBytes(32).toString('hex');
-}
 
-async function sendMail(userEmail, verificationToken) {
-  let mailOptions = {
-    from: 'konner.programming@gmail.com',
-    to: userEmail,
-    subject: 'Verify Your Email',
-    html: `
-      <p>Please click the link below to verify your email:</p>
-      <a href="${verificationLink}">Verify Email</a>
-      <p>If you did not request this, please ignore this email.</p>
-    `,
-  };
-
-  try {
-    let info = await transporter.sendMail(mailOptions);
-    console.log('Email sent: %s', info.messageId);
-  } catch (error) {
-    console.error('Error sending email:', error);
-  }
-}
