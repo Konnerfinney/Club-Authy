99c9321c02686ebed99b03ad3bae4409d46736fc
konnerfinney <kfinney@trinitytg.com>
2024-01-06 14:00:31 -0800

    Removal of pages

--- app/test.ts
+++
@@ -1,9 +0,0 @@
-export default async function getUserID(){
-    const response = await fetch('https://discord.com/api/users/@me', {
-        headers: {
-            'Authorization': `Bearer ${process.env.DISCORD_ACCESS_TOKEN}`
-        }
-    })
-    const json = await response.json()
-    return json.id
-}
