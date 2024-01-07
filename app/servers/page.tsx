6d94298a38f3090cf0d1a4ac20f65817534a618a
konnerfinney <kfinney@trinitytg.com>
2024-01-07 14:55:33 -0800

    Changed text color to gray to increase contrast

--- app/servers/page.tsx
+++ app/servers/page.tsx
@@ -48,7 +48,7 @@ export default function ServersPage() {
   return (
     <div className="container mx-auto px-4">
       <section>
-        <h1 className="text-2xl font-bold text-gray-900 my-4">Moderated Guilds</h1>
+        <h1 className="text-2xl font-bold text-gray-300 my-4">Moderated Guilds</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {moderatedGuilds.map(guild => (
             <Link key={guild.id} href={`/servers/${guild.id}`} passHref>
@@ -63,7 +63,7 @@ export default function ServersPage() {
       </section>
       
       <section>
-        <h1 className="text-2xl font-bold text-gray-900 my-4">Unmoderated Guilds</h1>
+        <h1 className="text-2xl font-bold text-gray-300 my-4">Unmoderated Guilds</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {unmoderatedGuilds.map(guild => (
             <div key={guild.id} className="bg-white rounded-lg shadow-md p-4">
